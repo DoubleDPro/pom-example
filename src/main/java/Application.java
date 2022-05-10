09e40a6dfd788c59547f181ea2a4aa298803acc2
Усачев Дмитрий Дмитриевич <92usachevdd@region.cbr.ru>
2022-05-10 12:28:51 +0300

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+public class Application {
+
+  public static void main(String[] args) {
+    boolean condition = false;
+    int i = 10;
+
+    while (condition) {
+      i++;
+    }
+
+    System.out.println(i);
+  }
+
+}
